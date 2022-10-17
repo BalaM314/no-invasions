abf8f72e2f6d8b4c2adac5cb677bd097ce53d16f
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-10-17 18:54:50 +0530

    V7 no longer requires the event shenanigans

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
 //When a sector is captured
-Events.on(EventType.SectorCaptureEvent, event => {
-	//Tell the sector to not have spawns
-	event.sector.info.hasSpawns = false;
-});
+Planets.serpulo.allowSectorInvasion = false;
+Log.info("[No Invasions] Disabled sector invasions on Serpulo.");
